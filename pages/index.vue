1568e27fefda6c92d8ac5dba34cb1ae9a9be59b9
donPuerto <don.puerto@hotmail.com>
2023-09-25 18:45:45 +0800

    ..

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { defineComponent } from 'vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="
+      text-3xl
+    ">
+      Home
+    </h1>
+  </div>
+</template>
